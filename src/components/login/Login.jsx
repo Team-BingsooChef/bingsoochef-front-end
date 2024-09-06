e3b9521cad655a068879bbecff7767dd06fd74bc
이동윤 <sub09065@gmail.com>
2024-09-06 23:52:32 +0900

    09.06

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,38 @@
+import React, { useState } from "react";
 import styles from "./Login.module.css";
+import WrongLogin from "../modal/WrongLogin";
 import { useNavigate, Link } from "react-router-dom";
 import gotobackicon from "/src/assets/icon/gotobackicon.png";
 
 const Login = () => {
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState(""); 
+  const [isModalOpen, setModalOpen] = useState(false);
+
   const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   const goToBack = (e) => {
     e.preventDefault();
     window.history.back();
   };
+
   const goToCreate = () => {
     navigate("/prelogin/createaccount");
   };
+
   const goToMain = () => {
-    navigate("/main");
+    // 간단한 로그인 검증 로직 (실제로는 서버 요청으로 대체)
+    if (email === "bingsoochef2@gmail.com" && password === "password") {
+      navigate("/main");
+    } else {
+      setModalOpen(true); // 로그인 실패 시 모달 열기
+    }
   };
+
   return (
     <div className={styles.login_display}>
       <div className={styles.login_form}>
@@ -32,12 +50,16 @@ const Login = () => {
           className={styles.inpute}
           type="email"
           placeholder="이메일을 입력해 주세요"
+          value = {email}
+          onChange={(e) => setEmail(e.target.value)}
         ></input>
         <label className={styles.inputplabel}>비밀번호</label>
         <input
           className={styles.inputp}
           type="password"
           placeholder="비밀번호를 입력해 주세요"
+          value = {password}
+          onChange={(e) => setPassword(e.target.value)}
         ></input>
         <Link to="/findpassword" className={styles.findpw}>비밀번호 찾기</Link>
         <button className={styles.gomain} onClick={goToMain}>
@@ -49,6 +71,12 @@ const Login = () => {
           <label className={styles.gotocreate} onClick={goToCreate}>회원가입</label>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className={styles.modalDisplay} onClick={handleCloseModal}>
+        <WrongLogin isOpen={isModalOpen} onClose={handleCloseModal} />
+        </div>
+      )}
     </div>
   );
 };
