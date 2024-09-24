9681570f230dfdf025d8a8c8e3db003705c580a1
Heeyeun Ko <koheeyeun96@gmail.com>
2024-09-24 20:12:07 +0900

    feat: set ChefName 기초

--- src/components/modal/chef/SetChefName.jsx
+++ src/components/modal/chef/SetChefName.jsx
@@ -3,17 +3,19 @@ import { useNavigate } from "react-router-dom";
 import styles from "./SetChefName.module.css";
 
 
-const SetChefName = ({isOpen, onClose, from, content, isReplied, replyContent, willReply }) => {
+const SetChefName = ({isOpen, onClose, onChefNameSuccess}) => {
   if (!isOpen) return null;
 
-  
+  const handleChefName = () => {
+    onChefNameSuccess();
+  }
   return (
-  
+    <div className={styles.modalDisplay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
      
        ㅎㅇ
       </div>
-    
+    </div>
   );
 };
 
