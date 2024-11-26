fbe15120587c5f30e70bbe4c8f0b80a637d68b27
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-26 16:34:45 +0900

    env: TypeScript project로 이사

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# 빙수셰프
+## 빙수 셰프
+
 리팩토링 & API 연결 중이에요
 
 components 폴더 안에 auth, user, home을 제외하곤 다 없어질 예정
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+})
