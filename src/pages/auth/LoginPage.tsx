e80c39aecad51da947f779c548ddac6555a81af3
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-28 03:21:05 +0900

    Feat: IvoryInput 컴포넌트 작성

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,11 +1,16 @@
 import styled from "@emotion/styled";
 import { BlueRectangleButton } from "../../components/common/BlueRectangleButton";
-import { IvoryRectangleButton } from "../../components/common/IvoryRectangleButton";
+import { IvoryInput } from "../../components/common/IvoryInput";
 import { Button } from "@chakra-ui/react";
 export default function LoginPage() {
 
     return(
         <>
+        <IvoryInput           
+        value={email}
+          text="이메일"
+          handleChange={handleChangeEmail}
+          placeholder="이메일을 입력해주세요."" />
         <BlueRectangleButton>로그인</BlueRectangleButton>
         <h1>하이</h1>
         </>
