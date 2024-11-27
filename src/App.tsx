7080b8c47f61ecd15f0cd252e47b9a54e0211502
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-27 22:57:49 +0900

    chore:  패키지 매니저 pnpm으로 바꿈

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Router } from "./routes/Router.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
+    <ChakraProvider>
     <Router />
+    </ChakraProvider>
   );
 }
 
