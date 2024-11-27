487446bc91466d9d2a44886b50a535371df17b0d
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-27 20:36:05 +0900

    chore: env 파일 추가

---
+++ src/routes/Router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterPath } from "./path"; //경로 상수 가져옴
+
+import HomePage from "@/pages/home/HomPage";
+import LoginPage from "@/pages/auth/LoginPage";
+import SignUpPage from "@/pages/auth/SignUpPage";
+import FindPasswordPage from "@/pages/auth/FindPasswordPage";
+import EmailCheckPage from "@/pages/auth/EmailCheckPage";
+import AfterSignUpPage from "@/pages/auth/AfterSignUpPage";
+import ChangeFlavorPage from "../pages/user/ChangeFlavorPage";
+
+
---
+++ src/routes/path.ts
@@ -0,0 +1,13 @@
+export const RouterPath = {
+  root: "",
+  home: "/",
+  login: "/login",
+  signup: "/signup",
+  findpassword: "/findpassword",
+  emailcheck: "/emailcheck",
+  aftersignup: "/aftersignup",
+  setting: "/setting",
+  seetoppinglist: "/seetoppinglist",
+  search: "/search",
+  changeflavor: "changeflavor",
+};
