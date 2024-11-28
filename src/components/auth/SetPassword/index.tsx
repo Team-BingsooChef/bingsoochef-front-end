c228cddeb4630384565997e26b64ca34a1f4bf7f
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-28 16:56:01 +0900

    feat: SetPassword, ReSetPassword 컴포넌트 구현, navigate state 따라 뜨도록

--- src/components/auth/SetPassword/index.tsx
+++ src/components/auth/SetPassword/index.tsx
@@ -1,14 +1,98 @@
+import { ToppestText } from "../ToppestText";
+import { PasswordInput } from "../PasswordInput";
+import { BlueRectangleButton } from "../../common/CustomedButton";
+import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+
 export const SetPassword = () => {
+  const toast = useToast();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); // 두 번째 비밀번호
+
+  const handleSubmit = () => {
+    if (password !== confirmPassword) {
+      toast({
+        title: "비밀번호가 동일하지 않습니다.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    // 비밀번호가 동일한 경우 로직
+    toast({
+      title: "비밀번호가 설정되었습니다.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+
+    // 다음 단계로 이동 로직 추가
+  };
+
   return (
-<div>
-    ㅎㅇ
-</div>
+    <>
+      <ToppestText text="비밀번호 변경" />
+      <PasswordInput
+        value={password}
+        text="비밀번호"
+        placeholder="10~15자, 특수문자 포함"
+        handleChange={(e) => setPassword(e.target.value)}
+      />
+      <PasswordInput
+        value={confirmPassword}
+        text="비밀번호 확인"
+        placeholder="비밀번호를 한 번 더 입력해 주세요"
+        handleChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <BlueRectangleButton onClick={handleSubmit}>변경하기</BlueRectangleButton>
+    </>
   );
-}
+};
 export const ReSetPassword = () => {
-    return (
-  <div>
-      ㅎㅇ
-  </div>
-    );
-  }
+  const toast = useToast();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); // 두 번째 비밀번호
+
+  const handleSubmit = () => {
+    if (password !== confirmPassword) {
+      toast({
+        title: "비밀번호가 동일하지 않습니다.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    // 비밀번호가 동일한 경우 로직
+    toast({
+      title: "비밀번호가 재설정되었습니다.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+
+    // 다음 단계로 이동 로직 추가
+  };
+
+  return (
+    <>
+      <ToppestText text="가입하기" />
+      <PasswordInput
+        value={password}
+        text="비밀번호"
+        placeholder="10~15자, 특수문자 포함"
+        handleChange={(e) => setPassword(e.target.value)}
+      />
+      <PasswordInput
+        value={confirmPassword}
+        text="비밀번호 확인"
+        placeholder="비밀번호를 한 번 더 입력해 주세요"
+        handleChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <BlueRectangleButton onClick={handleSubmit}>다음으로</BlueRectangleButton>
+    </>
+  );
+};
