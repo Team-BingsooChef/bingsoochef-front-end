9b1dc033b6fe7c82c188bfbb579e03d9b51511d0
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-28 13:19:47 +0900

    style: route path에 따른 다른 배경 이미지 적용

--- src/pages/auth/EmailCheckPage.tsx
+++ src/pages/auth/EmailCheckPage.tsx
@@ -1,8 +1,16 @@
+import styled from "@emotion/styled";
 export default function EmailCheckPage() {
 
     return(
-        <>
+        <Wrapper>
         <h1>하이</h1>
-        </>
+        </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+  width: calc(100% - 60px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
