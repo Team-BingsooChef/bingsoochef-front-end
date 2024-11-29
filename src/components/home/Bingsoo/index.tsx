7ba5bad2e83ef82af7db065dd02cdfac12d3e49e
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-29 21:34:43 +0900

    fix: bingsoo img cover -> contain

--- src/components/home/Bingsoo/index.tsx
+++ src/components/home/Bingsoo/index.tsx
@@ -27,12 +27,13 @@ export const Bingsoo = () => {
 
 const BingsooContainer = styled.div<{ bgImage: string }>`
   width: 96%;
-  height: 67vh;
+  height: 66vh;
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 60px;
   background-image: url(${(props) => props.bgImage});
-  background-size: cover; /* 배경화면을 컨테이너에 맞게 조정 */
+  background-repeat: no-repeat; /* 배경화면 반복 없음 */
+  background-size: contain; /* 배경화면을 컨테이너에 맞게 조정 */
   background-position: center; /* 배경화면을 중앙 정렬 */
 `;
