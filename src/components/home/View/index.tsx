261939edef0e588d40a786b87ae441d567bcf996
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-30 03:31:15 +0900

    feat: ChefView에 본인 빙수로 가는 Home Button 추가

--- src/components/home/View/index.tsx
+++ src/components/home/View/index.tsx
@@ -2,7 +2,9 @@ import { Menu } from "../Menu";
 import { Bingsoo } from "../Bingsoo";
 import { ToppingsPagination } from "../FloatToppings";
 import { AddTopping, CopyLink } from "../HomeBottomButton";
-import { Text, Flex } from "@chakra-ui/react";
+import { Text, Flex, IconButton } from "@chakra-ui/react";
+import { House } from "lucide-react";
+
 export const OwnerView = () => {
     return ( 
         <>
@@ -22,15 +24,17 @@ export const ChefView = () => {
     const nickname = "희연이";
     return ( 
         <>
-        <Menu />
+          <Flex w="100%" ml="12px" mt="8px">
+        <IconButton icon={<House size={28}/>} variant="ghost" aria-label="goMine" color="#777C89"/>
+        </Flex>
         <Text mt="10px">사랑하는 {nickname}의 빙수를 완성시켜 주세요</Text>
         <Bingsoo />
         <Flex mt="4px" justify="center" alignItems="center" >
         <ToppingsPagination />
         </Flex>
         <AddTopping />
-
         {/* <Bingsoo viewType="owner" /> viewType 넣어서 topping 클릭 여부*/}
-     </>
+    </>
     )
 };
+
