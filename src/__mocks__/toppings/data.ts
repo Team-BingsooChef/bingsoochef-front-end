995839ac7a9341d3da3b59db2713bd68abb96fe2
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-29 23:38:03 +0900

    chore: topping mocks data 추가

---
+++ src/__mocks__/toppings/data.ts
@@ -0,0 +1,154 @@
+export toppingData = 
+[
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 0,
+          "toppingTypeId": 0,
+          "chefName": "rnvqhmsg",
+          "toppingTitle": "yjoflmvw",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 1,
+          "toppingTypeId": 1,
+          "chefName": "dscnhojl",
+          "toppingTitle": "kuvgqzxm",
+          "toppingPosition": 0,
+          "isHidden": false
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 2,
+          "toppingTypeId": 2,
+          "chefName": "bvxuqwza",
+          "toppingTitle": "cqlhfkop",
+          "toppingPosition": 0,
+          "isHidden": false
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 3,
+          "toppingTypeId": 3,
+          "chefName": "tgflnqpy",
+          "toppingTitle": "miwodrkc",
+          "toppingPosition": 0,
+          "isHidden": false
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 4,
+          "toppingTypeId": 4,
+          "chefName": "xpdrznvf",
+          "toppingTitle": "qwmjklvc",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 5,
+          "toppingTypeId": 5,
+          "chefName": "yqkhnouj",
+          "toppingTitle": "vxqjdloh",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 6,
+          "toppingTypeId": 6,
+          "chefName": "kcjvuzsq",
+          "toppingTitle": "lvkrfwqa",
+          "toppingPosition": 0,
+          "isHidden": false
+        }
+      ]
+    },
+    {
+      "currPage": 0,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 7,
+          "toppingTypeId": 7,
+          "chefName": "fplsmxzw",
+          "toppingTitle": "cwqrjfkm",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    },
+    {
+      "currPage": 1,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 8,
+          "toppingTypeId": 8,
+          "chefName": "gjkmqrcn",
+          "toppingTitle": "bzlqsmfw",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    },
+    {
+      "currPage": 1,
+      "totalPages": 2,
+      "totalElements": 10,
+      "toppings": [
+        {
+          "toppingId": 9,
+          "toppingTypeId": 9,
+          "chefName": "txnrcyjo",
+          "toppingTitle": "klpwvzsm",
+          "toppingPosition": 0,
+          "isHidden": true
+        }
+      ]
+    }
+  ]
+  
