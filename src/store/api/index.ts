987df5b13dbb5fdc26d330bb91635a5b5aae1bae
Heeyeun Ko <koheeyeun96@gmail.com>
2024-12-01 04:23:06 +0900

    feat: 토핑 생성 requestBody를 담을 store 생성

--- src/store/api/index.ts
+++ src/store/api/index.ts
@@ -1,39 +1,8 @@
-import { create } from "zustand";
+
 import { CreateToppingRequestBody } from "../../api/bingsoo/types";
+import { create } from "zustand";
 
-type ModalOpenState = {
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
-  };
-  
-  export const useModalOpenStore = create<ModalOpenState>((set) => ({
-    isOpen: false, // 초기 모달 상태
-    onOpen: () => set({ isOpen: true }), // 모달 열기 함수
-    onClose: () => set({ isOpen: false }), // 모달 닫기 함수
-  }));
-  
-  type ModalHeightState = {
-    height: string;
-    setHeight: (height: string) => void;
-  };
-  
-  export const useModalHeightStore = create<ModalHeightState>((set) => ({
-    height: "70%", // 초기 모달 높이
-    setHeight: (height) => set({ height }),
-  }));
-  
-  type ModalStateStore = {
-    modalState: string;
-    setModalState: (newState: string) => void;
-  }
-  
-  export const useModalStateStore = create<ModalStateStore>((set) => ({
-    modalState: "selectTopping",
-    setModalState: (newState) => set({ modalState: newState }),
-  }));
-
-  interface CreateToppingStore {
+interface CreateToppingStore {
     requestBody: CreateToppingRequestBody;
     setUserId: (userId: number) => void;
     setBingsooId: (bingsooId: number) => void;
