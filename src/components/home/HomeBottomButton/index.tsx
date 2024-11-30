a37dabc841e4a3a7e69c4cff89869f2082a90d75
Heeyeun Ko <koheeyeun96@gmail.com>
2024-11-30 23:39:43 +0900

    feat: onOpen 동작 확인

--- src/components/home/HomeBottomButton/index.tsx
+++ src/components/home/HomeBottomButton/index.tsx
@@ -1,14 +1,18 @@
 import { BlueRectangleButton } from "../../common/CustomedButton";
 import { Box } from "@chakra-ui/react";
+import { useModalOpenStore } from "../../../store";
+
 export const AddToppingButton = () => {
+  const { onOpen } = useModalOpenStore();
   return (
     <Box w="calc(100% - 80px)" mt="16px">
-      <BlueRectangleButton>토핑 추가하기</BlueRectangleButton>
+      <BlueRectangleButton onClick={onOpen}>토핑 추가하기</BlueRectangleButton>
     </Box>
   );
 };
 
 export const CopyLink = () => {
+
   return (
     <Box w="calc(100% - 80px)" mt="16px">
       <BlueRectangleButton>공유하기</BlueRectangleButton>
---
+++ src/components/home/ModalLayout/index.tsx
@@ -0,0 +1,35 @@
+import ReactDOM from "react-dom";
+import styled from "@emotion/styled";
+import { useModalOpenStore } from '../../../store/index';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Image } from "@chakra-ui/react";
+
+export interface ModalLayoutProps {
+    height: string;
+    children?: React.ReactNode;
+}
+
+// export const ModalLayout = ({height, children}: ModalLayoutProps) => {
+export const ModalLayout = () => {
+    const {isOpen, onClose} = useModalOpenStore();
+    if (!isOpen) return null;
+    //포탈 박스
+    const portalElement = document.getElementById("portal");
+      // 포탈 박스 없으면 null
+  if (!portalElement) return null;
+  // 포탈 박스 있으면 이동
+  return ReactDOM.createPortal(
+        <>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Image src="https://bit.ly/sage-adebayo" alt="Segun Adebayo" />
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>,
+        portalElement
+  );
+}
