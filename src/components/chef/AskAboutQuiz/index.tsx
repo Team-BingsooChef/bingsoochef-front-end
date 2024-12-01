7e002983bde044fae3c551b73ca5e0d98334e1a1
Heeyeun Ko <koheeyeun96@gmail.com>
2024-12-02 03:33:25 +0900

    feat: CreateToppingStore에서 quizType, questions set하는 함수 분리

--- src/components/chef/AskAboutQuiz/index.tsx
+++ src/components/chef/AskAboutQuiz/index.tsx
@@ -1,14 +1,24 @@
 import { useModalHeight } from "../../../hook/useModalHeight";
+import { useModalStateStore } from "../../../store/modal";
 import {Text, Flex} from "@chakra-ui/react";
 import { BlueEllipseButton } from "../../common/CustomedButton";
+
 export const QuizOrNot = () => {
+    const { setModalState } = useModalStateStore();
     useModalHeight("24%"); 
+
+    const goSetChefName = () => {
+        setModalState("setChefName");
+        };
+    const goSelectQuizType = () => {
+        setModalState("selectQuizType");
+        };
     return (
     <>
     <Text color="white" fontSize="24px">퀴즈를 만드시겠습니까?</Text>
     <Flex w="calc(100% - 90px)" gap="24px" mt="20px">
-    <BlueEllipseButton>예</BlueEllipseButton>
-    <BlueEllipseButton>아니요</BlueEllipseButton>
+    <BlueEllipseButton onClick={goSelectQuizType}>예</BlueEllipseButton>
+    <BlueEllipseButton onClick={goSetChefName}>아니요</BlueEllipseButton>
     </Flex>
     </>
     );
--- src/store/api/topping/index.ts
+++ src/store/api/topping/index.ts
@@ -1,56 +1,71 @@
-
 import { CreateToppingRequestBody } from "../../../api/topping/types";
 import { create } from "zustand";
 
 interface CreateToppingStore {
-    requestBody: CreateToppingRequestBody;
-    setUserId: (userId: number) => void;
-    setBingsooId: (bingsooId: number) => void;
-    setTopping: (topping: CreateToppingRequestBody["topping"]) => void;
-    setToppingTypeId: (toppingTypeId: number) => void;
-    setQuiz: (quiz: CreateToppingRequestBody["quiz"]) => void;
-    setToppingContent: (toppingContent: string) => void; // toppingContent 업데이트 메서드
+  requestBody: CreateToppingRequestBody;
+  setUserId: (userId: number) => void;
+  setBingsooId: (bingsooId: number) => void;
+  setTopping: (topping: CreateToppingRequestBody["topping"]) => void;
+  setToppingTypeId: (toppingTypeId: number) => void;
+  setQuiz: (quiz: CreateToppingRequestBody["quiz"]) => void;
+  setQuizType: (quizType: string) => void; // quizType 업데이트 메서드
+  setQuestions: (questions: string[]) => void; // questions 업데이트 메서드
+  setToppingContent: (toppingContent: string) => void; // toppingContent 업데이트 메서드
   setChefName: (chefName: string) => void; // chefName 업데이트 메서드
-  }
+}
 
-  export const useCreateToppingStore = create<CreateToppingStore>((set) => ({
-    requestBody: {
-      userId: 0,
-      bingsooId: 0,
-      topping: {
-        chefName: "",
-        toppingTitle: "",
-        toppingContent: "",
-      },
-      toppingTypeId: 0,
-      quiz: {
-        quizTitle: "",
-        quizType: "OX",
-        questions: [],
-      },
+export const useCreateToppingStore = create<CreateToppingStore>((set) => ({
+  requestBody: {
+    userId: 0,
+    bingsooId: 0,
+    topping: {
+      chefName: "",
+      toppingTitle: "",
+      toppingContent: "",
     },
-    setUserId: (userId) => set((state) => ({ requestBody: { ...state.requestBody, userId } })),
-    setBingsooId: (bingsooId) =>
-      set((state) => ({ requestBody: { ...state.requestBody, bingsooId } })),
-    setTopping: (topping) =>
-      set((state) => ({ requestBody: { ...state.requestBody, topping } })),
-    setToppingTypeId: (toppingTypeId) =>
-      set((state) => ({ requestBody: { ...state.requestBody, toppingTypeId } })),
-    setQuiz: (quiz) =>
-      set((state) => ({ requestBody: { ...state.requestBody, quiz } })),
-    setToppingContent: (toppingContent) =>
-      set((state) => ({
-        requestBody: {
-          ...state.requestBody,
-          topping: { ...state.requestBody.topping, toppingContent },
-        },
-      })), // toppingContent만 업데이트
-    setChefName: (chefName) =>
-      set((state) => ({
-        requestBody: {
-          ...state.requestBody,
-          topping: { ...state.requestBody.topping, chefName },
-        },
-      })), // chefName만 업데이트
- 
-  }));
+    toppingTypeId: 0,
+    quiz: {
+      quizTitle: "",
+      quizType: "OX",
+      questions: [],
+    },
+  },
+  setUserId: (userId) =>
+    set((state) => ({ requestBody: { ...state.requestBody, userId } })),
+  setBingsooId: (bingsooId) =>
+    set((state) => ({ requestBody: { ...state.requestBody, bingsooId } })),
+  setTopping: (topping) =>
+    set((state) => ({ requestBody: { ...state.requestBody, topping } })),
+  setToppingTypeId: (toppingTypeId) =>
+    set((state) => ({ requestBody: { ...state.requestBody, toppingTypeId } })),
+  setQuiz: (quiz) =>
+    set((state) => ({ requestBody: { ...state.requestBody, quiz } })),
+  setQuizType: (quizType) =>
+    set((state) => ({
+      requestBody: {
+        ...state.requestBody,
+        quiz: { ...state.requestBody.quiz, quizType },
+      },
+    })), // quizType만 업데이트
+  setQuestions: (questions) =>
+    set((state) => ({
+      requestBody: {
+        ...state.requestBody,
+        quiz: { ...state.requestBody.quiz, questions },
+      },
+    })), // questions만 업데이트
+  setToppingContent: (toppingContent) =>
+    set((state) => ({
+      requestBody: {
+        ...state.requestBody,
+        topping: { ...state.requestBody.topping, toppingContent },
+      },
+    })), // toppingContent만 업데이트
+  setChefName: (chefName) =>
+    set((state) => ({
+      requestBody: {
+        ...state.requestBody,
+        topping: { ...state.requestBody.topping, chefName },
+      },
+    })), // chefName만 업데이트
+}));
