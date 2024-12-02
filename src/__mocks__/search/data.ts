5081178767b8c78b0676fd7e96e09dbc349e134a
Heeyeun Ko <koheeyeun96@gmail.com>
2024-12-02 17:32:59 +0900

    feat: 검색 필터링 적용 및 VSstack으로 결과 띄우기

---
+++ src/__mocks__/search/data.ts
@@ -0,0 +1,10 @@
+
+
+  // 사용자 목록 (임시 데이터)
+export const users = 
+[
+    { userId: 1, nickname: "호랭이", profileImg: "/src/assets/icon/bingsoo_icon.png" },
+    { userId: 2, nickname: "빙수왕", profileImg: "/src/assets/icon/bingsoo_icon.png" },
+    { userId: 3, nickname: "쿨빙수러버", profileImg: "/src/assets/icon/bingsoo_icon.png" },
+    { userId: 4, nickname: "냉장고", profileImg: "/src/assets/icon/bingsoo_icon.png" }
+ ] ;
